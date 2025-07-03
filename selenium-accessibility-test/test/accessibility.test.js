3fe5c3f6bb1268754d693f7aadf500279bfe7c6c
Andi Pilani <44569328+andi064@users.noreply.github.com>
2025-07-03 15:10:39 +0200

    Update js, axe core not running

--- selenium-accessibility-test/test/accessibility.test.js
+++ selenium-accessibility-test/test/accessibility.test.js
@@ -1,14 +1,14 @@
- const { Builder, By } = require('selenium-webdriver');
-const { expect } = require('chai');
-const axe = require('@axe-core/webdriverjs');
-const mocha = require('mocha');
+const { Builder, By } = require('selenium-webdriver'); // Import classes from Selenium WebDriver Builder and By to control the browser
+const { expect } = require('chai'); // expect assertion from npm modules
+const AxeBuilder = require('@axe-core/webdriverjs').default; 
+const mocha = require('mocha'); //import mocha to run the test from npm modules
 
 describe('Mars Demo Accessibility Tests', function () {
-  this.timeout(30000); // extend timeout for Selenium
+  this.timeout(30000); //default time out for mocha is 2 secs extend to 30 secs
 
   let driver;
 
-  before(async () => {
+  before(async () => { //hook function from mocha to decide what runs first and after
     driver = await new Builder().forBrowser('chrome').build();
     await driver.get('https://dequeuniversity.com/demo/mars');
   });
@@ -17,17 +17,16 @@ describe('Mars Demo Accessibility Tests', function () {
     await driver.quit();
   });
 
-  it('should load the main-nav element', async () => {
+  it('should load the main-nav element', async () => { //it is used to define the test case
     const nav = await driver.findElement(By.css('.main-nav'));
     const isDisplayed = await nav.isDisplayed();
     expect(isDisplayed).to.be.true;
   });
 
   it('should have no accessibility violations', async () => {
-    await axe(driver).analyze().then((results) => {
-      console.log('Accessibility Violations:', results.violations);
-      expect(results.violations.length).to.equal(0);
-    });
+    const builder = new AxeBuilder(driver);
+    const results = await builder.analyze();
+    console.log('Accessibility Violations:', results.violations);
+    expect(results.violations.length).to.equal(0);
   });
 });
-
